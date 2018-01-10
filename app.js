b548b12332d49123c4291f622a229c1bc6bc9ff7
MaxDrljic <max_spree@hotmail.com>
2018-01-10 22:00:01 +0100

    Setting up routes and token authorization

--- app.js
+++ app.js
@@ -9,6 +9,38 @@ app.get('/api', (req, res) => {
     });
 });
 
+app.post('/api/posts', verifyToken, (req, res) => {
+    res.json({
+        message: 'Post created'
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    // Mock user
+    const user = {
+        id: 1,
+        username: 'Max',
+        email: 'max@gmail.com'
+    }
+
+    jwt.sign({ user }, 'secretkey', (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
+// Format of Token
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    
+}
+
 app.listen(5000, () => {
     console.log('Server started on port 5000');
 });
